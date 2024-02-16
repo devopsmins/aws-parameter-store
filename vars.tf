3852e29903458983a5487c1d0f983565814801dd
minhaj <minhajfathima1023@gmail.com>
2024-02-16 06:43:19 -0500

    Prometheus ansible change

--- vars.tf
+++ vars.tf
@@ -2,7 +2,7 @@ variable "parameters"{
 
   default = [
   { name = "ses.username" , type = "String" ,value = "AKIA4BRQSNV6YJ4ODV5R" },
-    { name = "dev.rds.username" , type = "SecureString" ,value = "ExpenseApp1234" },
+    { name = "dev.rds.username" , type = "SecureString" ,value = "admin1" },
   { name = "dev.backend.DB_Host" , type = "String" ,value = "dev-mysql-rds.cmscnppwjzuf.us-east-1.rds.amazonaws.com" },
     { name = "dev.rds.endpoint" , type = "String" ,value = "dev-mysql-rds.cmscnppwjzuf.us-east-1.rds.amazonaws.com" },
 
