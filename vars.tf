594c7f18f0e03a70684275bd04c1f189a33e99b9
minhaj <minhajfathima1023@gmail.com>
2024-03-13 06:09:35 -0400

    TF code

--- vars.tf
+++ vars.tf
@@ -11,6 +11,6 @@ variable "parameters"{
 #Passwords
 { name = "dev.rds.password" , type = "SecureString" ,value = "ExpenseApp1234" },
     { name = "sonar.token" , type = "SecureString" ,value = "squ_46c27c38e5a04f2cf22e61f33a7464c2ac7fcfa8" },
-    { name = "artifactory-password" , type = "SecureString" ,value = "Admin123" }
+    { name = "artifactory.password" , type = "SecureString" ,value = "Admin123" }
   ]
 }
