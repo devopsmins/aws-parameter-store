8c77ead945f5a6da87758ff28ca06a53f245aa1b
minhaj <minhajfathima1023@gmail.com>
2024-03-15 05:55:07 -0400

    TF code

--- vars.tf
+++ vars.tf
@@ -6,11 +6,14 @@ variable "parameters"{
   { name = "dev.backend.DB_HOST" , type = "String" ,value = "dev-mysql-rds.cr60yyggs572.us-east-1.rds.amazonaws.com" },
     { name = "dev.frontend.BACKEND_ENDPOINT" , type = "String" ,value = "http://backend-dev.devopsmins.online/" },
     { name = "dev.rds.endpoint" , type = "String" ,value = "dev-mysql-rds.cr60yyggs572.us-east-1.rds.amazonaws.com" },
+    { name = "ssh.username" , type = "String" ,value = "root" } ,
 
 
 #Passwords
 { name = "dev.rds.password" , type = "SecureString" ,value = "ExpenseApp1234" },
     { name = "sonar.token" , type = "SecureString" ,value = "squ_46c27c38e5a04f2cf22e61f33a7464c2ac7fcfa8" },
-    { name = "artifactory.password" , type = "SecureString" ,value = "Admin123" }
+    { name = "artifactory.password" , type = "SecureString" ,value = "Admin123" } ,
+
+    { name = "ssh.password" , type = "SecureString" ,value = "DevOps321" },
   ]
 }
